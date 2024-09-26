bc23104077fd428c77f975797567dff2611b53ce
Eeeegarden <106652275+Eeeegarden@users.noreply.github.com>
2024-09-26 16:50:19 +0900

    setup : Spring 초기 파일 업로드 #3

---
+++ BE/ytt/src/main/java/com/example/ytt/YttApplication.java
@@ -0,0 +1,13 @@
+package com.example.ytt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YttApplication.class, args);
+	}
+
+}
