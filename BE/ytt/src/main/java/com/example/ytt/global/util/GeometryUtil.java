d9e82685e9657eeb8b3f8f40628226b214746f12
LEE JUNE <mars102aq@gmail.com>
2024-10-10 17:49:03 +0900

    feat: 위도,경도 생성을 위한 유틸리티 클래스 생성 #13

---
+++ BE/ytt/src/main/java/com/example/ytt/global/util/GeometryUtil.java
@@ -0,0 +1,27 @@
+package com.example.ytt.global.util;
+
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Point;
+
+/**
+ * Geometry 유틸리티 클래스
+ */
+public class GeometryUtil {
+
+    // private 생성자: 인스턴스화를 방지
+    private GeometryUtil() {
+        throw new UnsupportedOperationException("Utility Class");
+    }
+
+    /**
+     * 위도(latitude)와 경도(longitude)를 받아 Point 객체를 생성하여 반환
+     * @param latitude 위도
+     * @param longitude 경도
+     * @return Point
+     */
+    public static Point createPoint(final Double latitude, final Double longitude) {
+        final GeometryFactory geometryFactory = new GeometryFactory();
+        return geometryFactory.createPoint(new Coordinate(longitude, latitude));
+    }
+}
