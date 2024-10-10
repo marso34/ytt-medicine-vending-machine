6bbfc1be7dcc025fcd87c0f1748b443891605d0c
LEE JUNE <mars102aq@gmail.com>
2024-10-10 17:49:26 +0900

    feat: 자판기 테스트 코드 작성 #13

---
+++ BE/ytt/src/test/java/com/example/ytt/domain/vendingmachine/domain/VendingMachineTest.java
@@ -0,0 +1,94 @@
+package com.example.ytt.domain.vendingmachine.domain;
+
+import com.example.ytt.domain.model.Address;
+import com.example.ytt.global.util.GeometryUtil;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class VendingMachineTest {
+
+    private VendingMachine vendingMachine;
+    private Address address;
+
+    @BeforeEach
+    void setUp() {
+         address = Address.builder()
+                .addressDetails("address")
+                .location(GeometryUtil.createPoint(37.123456, 127.123456))
+                .build();
+
+        vendingMachine = VendingMachine.builder()
+                .name("name")
+                .address(address)
+                .state(MachineState.OPERATING)
+                .capacity(10)
+                .build();
+    }
+
+    @DisplayName("자판기 생성 테스트")
+    @Test
+    void createVendingMachine() {
+        assertThat(vendingMachine).isNotNull();
+        assertThat(vendingMachine.getName()).isEqualTo("name");
+        assertThat(vendingMachine.getAddress()).isEqualTo(address);
+        assertThat(vendingMachine.getState()).isEqualTo(MachineState.OPERATING);
+        assertThat(vendingMachine.getCapacity()).isEqualTo(10);
+    }
+
+    @DisplayName("자판기 이름 수정 테스트")
+    @Test
+    void updateVendingMachineName() {
+        vendingMachine.updateName("new name");
+
+        assertThat(vendingMachine.getName()).isEqualTo("new name");
+    }
+
+    @DisplayName("자판기 주소 수정 테스트")
+    @Test
+    void updateVendingMachineAddress() {
+        final Address newAddress = Address.builder()
+                .addressDetails("new address")
+                .location(GeometryUtil.createPoint(37.654321, 127.654321))
+                .build();
+
+        vendingMachine.updateAddress(newAddress);
+
+        assertThat(vendingMachine.getAddress()).isEqualTo(newAddress);
+    }
+
+    @DisplayName("자판기 상태 수정 테스트")
+    @Test
+    void updateVendingMachineState() {
+        vendingMachine.updateState(MachineState.MAINTENANCE);
+
+        assertThat(vendingMachine.getState()).isEqualTo(MachineState.MAINTENANCE);
+    }
+
+    @DisplayName("Equals & HashCode 테스트")
+    @Test
+    void equalsAndHashCode() {
+        final VendingMachine vendingMachine2 = VendingMachine.builder()
+                .name("name")
+                .address(address)
+                .state(MachineState.OPERATING)
+                .capacity(10)
+                .build();
+
+        System.out.println(vendingMachine.hashCode());
+
+        assertThat(vendingMachine).isEqualTo(vendingMachine2);
+        assertThat(vendingMachine.hashCode()).hasSameHashCodeAs(vendingMachine2.hashCode());
+    }
+
+    @DisplayName("ToString 테스트")
+    @Test
+    void toStringTest() {
+        System.out.println(vendingMachine.toString());
+
+        assertThat(vendingMachine.toString()).contains("name", "address", "state", "capacity");
+    }
+
+}
