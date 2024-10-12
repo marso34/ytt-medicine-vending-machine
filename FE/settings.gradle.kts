db2c5bbc5f71f928f5ae2fd41af3a0f3abd96fc3
ylose <heungeob1003@gmail.com>
2024-10-12 17:46:34 +0900

    setup: 깃 이그노어 새로 적용 #14

--- FE/settings.gradle.kts
+++ FE/settings.gradle.kts
@@ -11,14 +11,15 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven(url = uri("https://devrepo.kakao.com/nexus/content/groups/public/"))
     }
 }
 
 rootProject.name = "ytt-android"
 include(":app")
- 
