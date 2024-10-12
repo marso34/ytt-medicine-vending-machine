e02a0150e7d51f750de3a7ba86428c327a831d65
ylose <heungeob1003@gmail.com>
2024-10-12 14:32:24 +0900

    feat: 지도 테스트 #14

--- FE/app/src/main/java/com/wonchihyeon/ytt_android/HomeFragment.kt
+++ FE/app/src/main/java/com/wonchihyeon/ytt_android/HomeFragment.kt
@@ -1,10 +1,20 @@
 package com.wonchihyeon.ytt_android
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.google.android.gms.maps.MapView
+import com.kakao.vectormap.KakaoMap
+import com.kakao.vectormap.KakaoMapReadyCallback
+import com.kakao.vectormap.KakaoMapSdk
+import com.kakao.vectormap.LatLng
+import com.kakao.vectormap.MapLifeCycleCallback
+import com.wonchihyeon.ytt_android.databinding.FragmentHomeBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,43 +27,66 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var binding: FragmentHomeBinding
+
+    private var _binding : FragmentHomeBinding? = null
+
+    private lateinit var mapView : MapView
+    private var kakaoMap : KakaoMap? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        showMapView()
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+
+        binding.FindTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_findFragment)
+        }
+
+        showMapView()
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun showMapView(){
+
+        // KakaoMapSDK 초기화!!
+        KakaoMapSdk.init(requireContext(), KAKAO_MAP_KEY)
+
+
+        binding.mapView.start(object : MapLifeCycleCallback() {
+
+            override fun onMapDestroy() {
+                // 지도 API가 정상적으로 종료될 때 호출
+                Log.d("KakaoMap", "onMapDestroy")
             }
+
+            override fun onMapError(p0: Exception?) {
+                // 인증 실패 및 지도 사용 중 에러가 발생할 때 호출
+                Log.e("KakaoMap", "onMapError")
+            }
+        }, object : KakaoMapReadyCallback(){
+            override fun onMapReady(kakaomap: KakaoMap) {
+                // 정상적으로 인증이 완료되었을 때 호출
+                kakaoMap = kakaomap
+            }
+        })
     }
+
 }
