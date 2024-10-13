66d3e296f4d7914805763762595446456515df65
ylose <heungeob1003@gmail.com>
2024-10-13 11:12:39 +0900

    Fixed untracked files

---
+++ FE/app/src/main/java/com/wonchihyeon/ytt_android/account/FindPasswordActivity.kt
@@ -0,0 +1,16 @@
+package com.wonchihyeon.ytt_android.account
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.wonchihyeon.ytt_android.R
+
+class FindPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_find_password)
+    }
+}
