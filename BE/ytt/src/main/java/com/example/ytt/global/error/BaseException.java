c586145b1c41bb90d9fb3c14accaa4e581a7ace1
LEE JUNE <mars102aq@gmail.com>
2024-10-18 17:35:41 +0900

    refactor: exception 생성자 수정 #28

--- BE/ytt/src/main/java/com/example/ytt/global/error/BaseException.java
+++ BE/ytt/src/main/java/com/example/ytt/global/error/BaseException.java
@@ -1,5 +1,10 @@
 package com.example.ytt.global.error;
 
 public abstract class BaseException extends RuntimeException{
+
+    protected BaseException(String message){
+        super(message);
+    }
+
     public abstract BaseExceptionType getExceptionType();
 }
