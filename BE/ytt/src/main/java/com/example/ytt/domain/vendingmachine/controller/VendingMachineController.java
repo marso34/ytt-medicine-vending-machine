3de9dcfa2670c1c520abcae44b12f24e4a1472e4
LEE JUNE <mars102aq@gmail.com>
2024-10-18 17:21:50 +0900

    refactor: swagger annotation 적용 #28

--- BE/ytt/src/main/java/com/example/ytt/domain/vendingmachine/controller/VendingMachineController.java
+++ BE/ytt/src/main/java/com/example/ytt/domain/vendingmachine/controller/VendingMachineController.java
@@ -3,12 +3,10 @@ package com.example.ytt.domain.vendingmachine.controller;
 import com.example.ytt.domain.vendingmachine.dto.VendingMachineDetailDto;
 import com.example.ytt.domain.vendingmachine.dto.VendingMachineDto;
 import com.example.ytt.domain.vendingmachine.service.VendingMachineFindService;
-import io.swagger.v3.oas.annotations.Operation;
+import com.example.ytt.global.common.annotation.SwaggerApi;
+import com.example.ytt.global.common.response.ResponseDto;
+import com.example.ytt.global.util.ResponseUtil;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -26,38 +24,36 @@ public class VendingMachineController {
 
     // TODO: path 경로 수정 필요 (현재 임의로 설정)
 
-    @Operation(summary = "전체 자판기 조회", description = "모든 자판기 리스트 조회")
-    @ApiResponse(responseCode = "200", description = "모든 자판기 조회 성공", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = VendingMachineDto.class))))
     @GetMapping("/all")
-    public ResponseEntity<List<VendingMachineDto>> getAllVendingMachines() {
-        return ResponseEntity.ok(vendingMachineFindService.getAllVendingMachines());
+    @SwaggerApi(summary = "전체 자판기 조회", description = "모든 자판기 리스트 조회", implementation = ResponseDto.class)
+    public ResponseEntity<ResponseDto<List<VendingMachineDto>>> getAllVendingMachines() {
+        return ResponseUtil.success(vendingMachineFindService.getAllVendingMachines());
     }
 
-    @Operation(summary = "이름으로 자판기 조회", description = "이름에 포함되어 있는 자판기 리스트 조회")
-    @ApiResponse(responseCode = "200", description = "모든 자판기 조회 성공", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = VendingMachineDto.class))))
     @GetMapping("/name")
-    public ResponseEntity<List<VendingMachineDto>> getVendingMachinesByName(@Parameter(description = "자판기 이름", example = "강릉원주대 자판기") @RequestParam("name") String name) {
-        return ResponseEntity.ok(vendingMachineFindService.getVendingMachinesByName(name));
+    @SwaggerApi(summary = "이름으로 자판기 조회", description = "이름에 포함되어 있는 자판기 리스트 조회", implementation = ResponseDto.class)
+    public ResponseEntity<ResponseDto<List<VendingMachineDto>>> getVendingMachinesByName(@Parameter(description = "자판기 이름", example = "강릉원주대 자판기") @RequestParam("name") String name) {
+        return ResponseUtil.success(vendingMachineFindService.getVendingMachinesByName(name));
     }
 
-    @Operation(summary = "주변 자판기 조회", description = "주어진 값으로부터 반경 2.5km 내 자판기 리스트 조회")
-    @ApiResponse(responseCode = "200", description = "모든 자판기 조회 성공", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = VendingMachineDto.class))))
     @GetMapping("/nearby")
-    public ResponseEntity<List<VendingMachineDto>> getVendingMachinesNearByLocation(
+    @SwaggerApi(summary = "주변 자판기 조회", description = "주어진 값으로부터 반경 2.5km 내 자판기 리스트 조회", implementation = ResponseDto.class)
+    public ResponseEntity<ResponseDto<List<VendingMachineDto>>>getVendingMachinesNearByLocation(
             @Parameter(description = "자판기의 위도", example = "37.305121")
             @RequestParam("latitude") Double latitude,
             @Parameter(description = "자판기의 경도", example = "127.922653")
             @RequestParam("longitude") Double longitude) {
 
         // TODO: 거리 파라미터 추가할지, 기본값 설정할지 고민 필요
+        // TODO: 위도, 경도 범위 제한 추가할지 고민 필요
+        // TODO: RequestDto로 변경할지 고민 필요
 
-        return ResponseEntity.ok(vendingMachineFindService.getVendingMachinesNearByLocation(latitude, longitude, 2500.0));
+        return ResponseUtil.success(vendingMachineFindService.getVendingMachinesNearByLocation(latitude, longitude, 2500.0));
     }
 
-    @Operation(summary = "자판기 ID로 조회", description = "자판기 ID로 자판기 조회")
-    @ApiResponse(responseCode = "200", description = "자판기 조회 성공", content = @Content(mediaType = "application/json", schema = @Schema(implementation = VendingMachineDetailDto.class)))
     @GetMapping("/{id}")
-    public ResponseEntity<VendingMachineDetailDto> getVendingMachineById(@PathVariable(value = "id") Long id) {
-        return ResponseEntity.ok(vendingMachineFindService.getVendingMachineDetail(id));
+    @SwaggerApi(summary = "자판기 ID로 조회", description = "자판기 ID로 자판기 조회", implementation = ResponseDto.class)
+    public  ResponseEntity<ResponseDto<VendingMachineDetailDto>> getVendingMachineById(@PathVariable(value = "id") Long id) {
+        return ResponseUtil.success(vendingMachineFindService.getVendingMachineDetail(id));
     }
 }
