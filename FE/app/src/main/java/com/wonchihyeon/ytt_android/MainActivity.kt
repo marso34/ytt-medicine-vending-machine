885d86674f55e87fa364b1aa7bd334d112a8d714
ylose <heungeob1003@gmail.com>
2024-10-21 20:52:10 +0900

    feat:위치 권한 문구 추가(거부 시 로그인 액티비티로 이동) #40

--- FE/app/src/main/java/com/wonchihyeon/ytt_android/MainActivity.kt
+++ FE/app/src/main/java/com/wonchihyeon/ytt_android/MainActivity.kt
@@ -1,8 +1,16 @@
 package com.wonchihyeon.ytt_android
 
+
+import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.wonchihyeon.ytt_android.R
+import com.wonchihyeon.ytt_android.account.LoginActivity
 import com.wonchihyeon.ytt_android.databinding.ActivityMainBinding
 import com.wonchihyeon.ytt_android.fragments.FavoriteFragment
 import com.wonchihyeon.ytt_android.fragments.HomeFragment
@@ -13,11 +21,19 @@ class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
     private var homeFragment: HomeFragment? = null // HomeFragment의 인스턴스를 저장할 변수
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000 // 요청 코드
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        // 위치 권한 확인
+        checkLocationPermission()
 
         setBottomNavigationView()
 
@@ -43,7 +59,7 @@ class MainActivity : AppCompatActivity() {
                         .replace(R.id.main_container, homeFragment!!)
                         .commit()
                     // 하단 다이얼로그 업데이트
-                    homeFragment?.updateBottomSheet("홈 화면입니다.")
+                    homeFragment?.updateBottomSheet("등록된 자판기 목록")
                     true
                 }
                 R.id.fragment_favorite -> {
@@ -52,7 +68,7 @@ class MainActivity : AppCompatActivity() {
                         homeFragment = HomeFragment()
                     }
                     // 하단 다이얼로그 업데이트
-                    homeFragment?.updateBottomSheet("즐겨찾기에서 돌아왔습니다.")
+                    homeFragment?.updateBottomSheet("즐겨찾기한 자판기 목록")
                     supportFragmentManager.beginTransaction()
                         .replace(R.id.main_container, homeFragment!!)
                         .commit()
@@ -73,5 +89,35 @@ class MainActivity : AppCompatActivity() {
                 else -> false
             }
         }
+    }
+
+    private fun checkLocationPermission() {
+        // 위치 권한 체크
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // 권한이 없으면 요청
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            LOCATION_PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // 권한이 허용됨
+                    // 위치 관련 작업 수행
+                } else {
+                    // 권한이 거부됨
+                    // 로그인 화면으로 이동
+                    navigateToLoginActivity()
+                }
+            }
+        }
+    }
+
+    private fun navigateToLoginActivity() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish() // 현재 액티비티 종료
     }
 }
