a1684d370cec446813cc8160ad5f744dc519288c
LEE JUNE <mars102aq@gmail.com>
2024-10-22 20:01:19 +0900

    refactor: ResponseDto code 원시 타입으로 변경

--- BE/ytt/src/main/java/com/example/ytt/global/common/response/ResponseDto.java
+++ BE/ytt/src/main/java/com/example/ytt/global/common/response/ResponseDto.java
@@ -4,9 +4,9 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(title = "response DTO", description = "응답 DTO")
 public record ResponseDto<T>(
-        @Schema(description = "code", example = "200")      Integer code,
-        @Schema(description = "message", example = "성공")  String message,
-        @Schema(description = "body")                       T body
+        @Schema(description = "code")     int code,
+        @Schema(description = "message")  String message,
+        @Schema(description = "body")     T body
 ) {
 
         public static <T> ResponseDto<T> of(int code, String message, T body) {
