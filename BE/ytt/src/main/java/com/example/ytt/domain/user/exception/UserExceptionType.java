b59ef7374040ae9fb74447cd7cf2fb2a9efbe07d
Eeeegarden <106652275+Eeeegarden@users.noreply.github.com>
2024-10-24 16:05:04 +0900

    refactor: 토큰관련 예외처리 추가 #21

--- BE/ytt/src/main/java/com/example/ytt/domain/user/exception/UserExceptionType.java
+++ BE/ytt/src/main/java/com/example/ytt/domain/user/exception/UserExceptionType.java
@@ -23,8 +23,13 @@ public enum UserExceptionType implements BaseExceptionType {
     SIGNUP_FORMAT_INVALID(604, HttpStatus.BAD_REQUEST, "유효성 에러."), // 400 Bad Request
     EMAIL_FORMAT_INVALID(605, HttpStatus.BAD_REQUEST, "이메일 형식이 올바르지 않습니다."), // 400 Bad Request
     PASSWORD_FORMAT_INVALID(606, HttpStatus.BAD_REQUEST, "비밀번호 형식이 올바르지 않습니다."), // 400 Bad Request
-    NOTBLANK_FORMAT_INVALID(607, HttpStatus.BAD_REQUEST, "공백이 존재할 수 없습니다."); // 400 Bad Request
-
+    NOTBLANK_FORMAT_INVALID(607, HttpStatus.BAD_REQUEST, "공백이 존재할 수 없습니다."), // 400 Bad Request
+    BLANK_REFRESH_TOKEN(608, HttpStatus.BAD_REQUEST, "리프레시 토큰이 비었습니다."), // 400 Bad Request
+    EXPIRED_REFRESH_TOKEN(609, HttpStatus.BAD_REQUEST, "리프레시 토큰이 만료되었습니다."), // 400 Bad Request
+    INVALID_TOKEN_CATEGORY(610, HttpStatus.BAD_REQUEST, "토큰 타입이 올바르지 않습니다."), // 400 Bad Request
+    INVALID_REFRESH_TOKEN(6111, HttpStatus.BAD_REQUEST, "유효하지 않은 리프레시 토큰입니다."), // 400 Bad Request
+    INVALID_JWT_TOKEN(612, HttpStatus.BAD_REQUEST, "유효하지 않은 JWT 토큰입니다."), // 400 Bad Request
+    JSON_PARSE_ERROR(3000, HttpStatus.BAD_REQUEST, "JSON 파싱 과정에서 에러가 발생했습니다.");
     private int errorCode;
     private HttpStatus httpStatus;
     private String errorMessage;
