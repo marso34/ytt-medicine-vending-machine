3b049918c8cdf520dc143d972d45c21bda15ad02
ylose <heungeob1003@gmail.com>
2024-10-27 17:36:31 +0900

    setup: User 데이터 클래스 생성 #55

--- FE/app/src/main/java/com/wonchihyeon/ytt_android/data/model/User.kt
+++ FE/app/src/main/java/com/wonchihyeon/ytt_android/data/model/User.kt
@@ -1,4 +1,18 @@
 package com.wonchihyeon.ytt_android.data.model
 
-class User {
+data class User(
+    val id: Int,
+    val email: String,
+    val password: String,
+    val name: String,
+    val phoneNumber: String,
+    val role: Role,
+)
+
+enum class Role {
+    CUSTOMER,
+    MANAGER,
+    ADMIN
 }
+
+
