db0aa954adfd951f83d623f03a8866ee2f1f0cc3
ylose <heungeob1003@gmail.com>
2024-10-27 17:35:26 +0900

    setup: Retrofit 인스턴스 생성 #55

--- FE/app/src/main/java/com/wonchihyeon/ytt_android/data/network/ApiService.kt
+++ FE/app/src/main/java/com/wonchihyeon/ytt_android/data/network/ApiService.kt
@@ -1,4 +1,22 @@
 package com.wonchihyeon.ytt_android.data.network
 
-class ApiService {
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+    private const val BASE_URL = "end_point"
+    private val retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(
+                OkHttpClient.Builder().addInterceptor(HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BODY
+                }).build()
+            )
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
 }
