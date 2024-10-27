ec8fffa593fa72c8c9a95f50e10dea05f9e19525
ylose <heungeob1003@gmail.com>
2024-10-27 17:35:54 +0900

    setup: Retrofit 사용을 위한 라이브러리 추가 #55

--- FE/app/build.gradle.kts
+++ FE/app/build.gradle.kts
@@ -77,8 +77,8 @@ dependencies {
     implementation("com.naver.maps:map-sdk:3.12.0")
 
     // Retrofit
-    implementation 'com.squareup.retrofit2:retrofit:2.6.0'
-    implementation 'com.squareup.retrofit2:converter-gson:2.6.0'
-    implementation 'com.google.code.gson:gson:2.8.6'
-    implementation 'com.squareup.okhttp3:logging-interceptor:3.11.0'
+    implementation("com.squareup.retrofit2:retrofit:2.6.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.6.0")
+    implementation("com.google.code.gson:gson:2.8.6")
+    implementation("com.squareup.okhttp3:logging-interceptor:3.11.0")
 }
