3b0ab600926c3d8567130fb76a7e4ff8ef2d1c16
ylose <heungeob1003@gmail.com>
2024-10-28 11:20:23 +0900

    setup: vendingmachine DTO 준비 #59

---
+++ FE/app/src/main/java/com/wonchihyeon/ytt_android/data/model/vendingmachineDTO.kt
@@ -0,0 +1,19 @@
+package com.wonchihyeon.ytt_android.data.model
+
+data class vendingmachineDTO(
+    val id : Int,
+    val name: String,
+    val state: status,
+    val address: String,
+    val latitutde: Number,
+    val longtitude: Number,
+)
+
+enum class status() {
+    OPERATING,
+    OUT_OF_STOCK,
+    ERROR,
+    WARNING,
+    MAINTENANCE,
+    UNKNOWN,
+}
