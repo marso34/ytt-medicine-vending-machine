2f5d5a0dffc50539f1bbfddc13603165360fcaf1
ylose <heungeob1003@gmail.com>
2024-10-28 11:40:54 +0900

    setup: vendingmachineDTO 수정 #59

--- FE/app/src/main/java/com/wonchihyeon/ytt_android/data/model/vendingmachine/vendingmachineDTO.kt
+++ FE/app/src/main/java/com/wonchihyeon/ytt_android/data/model/vendingmachine/vendingmachineDTO.kt
@@ -1,12 +1,17 @@
 package com.wonchihyeon.ytt_android.data.model.vendingmachine
 
+import android.graphics.Point
+import java.sql.Timestamp
+
 data class vendingmachineDTO(
     val id : Int,
     val name: String,
-    val state: status,
     val address: String,
-    val latitutde: Number,
-    val longtitude: Number,
+    val location: Point,
+    val capacity: Int,
+    val created_at: Timestamp,
+    val modified_at: Timestamp,
+    val state: status,
 )
 
 enum class status() {
