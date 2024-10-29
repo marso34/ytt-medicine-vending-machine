d35f1b4daba8241eef35774c75c75e47e0d5b579
ylose <heungeob1003@gmail.com>
2024-10-29 13:48:09 +0900

    setup: vendingmachineviewmode생성 #59

--- FE/app/src/main/java/com/wonchihyeon/ytt_android/viewmodel/VendingMachineViewModel.kt
+++ FE/app/src/main/java/com/wonchihyeon/ytt_android/viewmodel/VendingMachineViewModel.kt
@@ -1,4 +1,16 @@
 package com.wonchihyeon.ytt_android.viewmodel
 
-class VendingMachineViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import com.wonchihyeon.ytt_android.data.network.vendingmachineRetrofit
+import com.wonchihyeon.ytt_android.data.repository.VendingMachineRepository
+import kotlinx.coroutines.Dispatchers
+
+class VendingMachineViewModel : ViewModel() {
+    private val repository = VendingMachineRepository(vendingmachineRetrofit.service)
+
+    val vendingMachines = liveData(Dispatchers.IO) {
+        val result = repository.fetchAllVendingMachines()
+        emit(result)
+    }
 }
