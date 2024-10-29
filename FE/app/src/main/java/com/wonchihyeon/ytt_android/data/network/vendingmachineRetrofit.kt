aeda9d3ae29e6b6e6e7f265b045846c1e782f04d
ylose <heungeob1003@gmail.com>
2024-10-29 13:48:43 +0900

    setup: vendingmachine Retrofit 생성 #59

--- FE/app/src/main/java/com/wonchihyeon/ytt_android/data/network/vendingmachineRetrofit.kt
+++ FE/app/src/main/java/com/wonchihyeon/ytt_android/data/network/vendingmachineRetrofit.kt
@@ -5,18 +5,14 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-// 전체 목록 조회하는 자판기 Retrofit
 object vendingmachineRetrofit {
-    private const val BASE_URL = "http://13.125.128.15:8080/vending-machine/all"
-    private val retrofit by lazy {
+    private const val BASE_URL = "/vending-machine/all"
+
+    val service: VendingMachineService by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .client(
-                OkHttpClient.Builder().addInterceptor(HttpLoggingInterceptor().apply {
-                    level = HttpLoggingInterceptor.Level.BODY
-                }).build()
-            )
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+            .create(VendingMachineService::class.java)
     }
 }
