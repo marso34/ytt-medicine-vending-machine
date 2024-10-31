cdecd0e85e7c5bcd20813bade3972269f4351030
ylose <heungeob1003@gmail.com>
2024-10-31 10:10:54 +0900

    remove: 브런치와 관계없는 파일들 삭제

--- FE/app/src/main/java/com/wonchihyeon/ytt_android/data/repository/VendingMachineRepository.kt
+++
@@ -1,4 +0,0 @@
-package com.wonchihyeon.ytt_android.data.repository
-
-class VendingMachineRepository {
-}
--- FE/app/src/main/java/com/wonchihyeon/ytt_android/viewmodel/MedicineViewModel.kt
+++
@@ -1,4 +0,0 @@
-package com.wonchihyeon.ytt_android.viewmodel
-
-class MedicineViewModel {
-}
