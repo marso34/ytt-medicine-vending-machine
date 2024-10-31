a9a62cea2c55460bf6b9520998e22716a203397d
ylose <heungeob1003@gmail.com>
2024-10-31 11:17:52 +0900

    feat: signUp 레포지토리 #61

--- FE/app/src/main/java/com/wonchihyeon/ytt_android/data/repository/SignUpRepository.kt
+++ FE/app/src/main/java/com/wonchihyeon/ytt_android/data/repository/SignUpRepository.kt
@@ -1,33 +1,33 @@
-package com.example.app.data.repository
+package com.wonchihyeon.ytt_android.data.repository
 
+import android.content.Context
 import com.wonchihyeon.ytt_android.data.model.SignUpDTO
+import com.wonchihyeon.ytt_android.data.model.SignUpResponseDTO
 import com.wonchihyeon.ytt_android.data.network.ApiService
 import com.wonchihyeon.ytt_android.data.network.RetrofitAPI
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class SignUpRepository {
+class SignUpRepository(private val context: Context) {
 
-    private val apiService: ApiService = RetrofitAPI.getRetrofit().create(ApiService::class.java)
+    private val apiService: ApiService = RetrofitAPI.getRetrofit(context).create(ApiService::class.java)
 
-    fun signUp(signUpDTO: SignUpDTO, callback: (String) -> Unit) {
-        apiService.signUp(signUpDTO).enqueue(object : Callback<String> {
-            override fun onResponse(call: Call<String>, response: Response<String>) {
-                // 응답 코드 확인
-                val responseCode = response.code()
+    fun signUp(signUpDTO: SignUpDTO, callback: (String, String?, String?) -> Unit) {
+        apiService.signUp(signUpDTO).enqueue(object : Callback<SignUpResponseDTO> {
+            override fun onResponse(call: Call<SignUpResponseDTO>, response: Response<SignUpResponseDTO>) {
                 if (response.isSuccessful) {
-                    // 성공적인 응답
-                    callback("회원가입 성공: ${response.body()} (응답 코드: $responseCode)")
+                    val responseBody = response.body()
+                    val accessToken = responseBody?.accessToken
+                    val refreshToken = responseBody?.refreshToken
+                    callback("회원가입 성공", accessToken, refreshToken)
                 } else {
-                    // 실패한 응답
-                    callback("회원가입 실패: ${response.message()} (응답 코드: $responseCode)")
+                    callback("회원가입 실패: ${response.message()}", null, null)
                 }
             }
 
-            override fun onFailure(call: Call<String>, t: Throwable) {
-                // 요청 실패
-                callback("회원가입 요청 실패: ${t.message}")
+            override fun onFailure(call: Call<SignUpResponseDTO>, t: Throwable) {
+                callback("회원가입 요청 실패: ${t.message}", null, null)
             }
         })
     }
