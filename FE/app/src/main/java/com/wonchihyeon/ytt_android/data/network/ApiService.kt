addd5542b95481918424554916e151c3435d5536
ylose <heungeob1003@gmail.com>
2024-10-31 19:02:06 +0900

    feat: ApiService 생성 #61

--- FE/app/src/main/java/com/wonchihyeon/ytt_android/data/network/ApiService.kt
+++ FE/app/src/main/java/com/wonchihyeon/ytt_android/data/network/ApiService.kt
@@ -1,16 +1,55 @@
 package com.wonchihyeon.ytt_android.data.network
 
+import com.google.firebase.crashlytics.buildtools.reloc.javax.annotation.Nullable
 import com.wonchihyeon.ytt_android.data.model.SignInDTO
 import com.wonchihyeon.ytt_android.data.model.SignUpDTO
-import com.wonchihyeon.ytt_android.data.model.SignUpResponseDTO
+import com.wonchihyeon.ytt_android.data.model.ResponseDTO
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface ApiService {
 
+    /*@GET("/medicine/{id}")
+
+    @GET("/medicine/productCode")
+
+    @GET("/medicine/name")
+
+    @GET("/medicine/manufacturer")
+
+    @GET("/medicine/ingredient")
+
+    @GET("/medicine/all")
+
+    @GET("/medicine/test")
+
+    @GET("/user/mypage")
+
+    @POST("/user/password")
+
+    @POST("/user/logout")*/
+
+   /* @POST("/vending-machine/create")
+
+    @POST("/vending-machine/add-medicine")
+
+    @POST("/auth/reissue")
+
+    @GET("/vending-machine/{id}")
+
+    @GET("/vending-machine/nearby")
+
+    @GET("/vending-machine/name")
+
+    @GET("/vending-machine/medicine")
+
+    @GET("/vending-machine/all")*/
+
+
     @POST("/user/signUp")
-    fun signUp(@Body signUpDTO: SignUpDTO): Call<SignUpResponseDTO>
+    fun signUp(@Body signUpDTO: SignUpDTO): Call<String>
 
 
     @POST("/user/signIn")
