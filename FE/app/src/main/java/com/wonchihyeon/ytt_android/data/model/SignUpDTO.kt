fba05ccffe5075b24daf7c448f261daab7c4dab2
ylose <heungeob1003@gmail.com>
2024-10-31 19:03:23 +0900

    feat: SignUpDTO 생성 #61

--- FE/app/src/main/java/com/wonchihyeon/ytt_android/data/model/SignUpDTO.kt
+++ FE/app/src/main/java/com/wonchihyeon/ytt_android/data/model/SignUpDTO.kt
@@ -5,7 +5,5 @@ data class SignUpDTO(
     val password: String,
     val name: String,
     val phoneNumber: String,
-    val role: Role,
-    val accessToken: String? = null,   // 서버 응답에서 포함되는 Access Token
-    val refreshToken: String? = null   // 서버 응답에서 포함되는 Refresh Token
+    val role: String,
 )
