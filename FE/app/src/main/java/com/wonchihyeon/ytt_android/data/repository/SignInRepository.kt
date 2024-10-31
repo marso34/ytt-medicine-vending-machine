91fdccf34e65fdbc48226050428b05bfe6602b3f
ylose <heungeob1003@gmail.com>
2024-10-31 11:18:46 +0900

    feat: 로그인 레포지토리 생성 #61

--- FE/app/src/main/java/com/wonchihyeon/ytt_android/data/repository/SignInRepository.kt
+++ FE/app/src/main/java/com/wonchihyeon/ytt_android/data/repository/SignInRepository.kt
@@ -1,5 +1,6 @@
 package com.wonchihyeon.ytt_android.data.repository
 
+import android.content.Context
 import com.wonchihyeon.ytt_android.data.model.SignInDTO
 import com.wonchihyeon.ytt_android.data.network.ApiService
 import com.wonchihyeon.ytt_android.data.network.RetrofitAPI
@@ -7,27 +8,23 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class SignInRepository {
+class SignInRepository(private val context: Context) {
 
-    private val apiService: ApiService = RetrofitAPI.getRetrofit().create(ApiService::class.java)
+    private val apiService: ApiService = RetrofitAPI.getRetrofit(context).create(ApiService::class.java)
 
-    fun signIn(signInDTO: SignInDTO, callback: (String) -> Unit) {
+    fun signIn(signInDTO: SignInDTO, callback: (String, String?) -> Unit) {
         apiService.signIn(signInDTO).enqueue(object : Callback<String> {
             override fun onResponse(call: Call<String>, response: Response<String>) {
-                // 응답 코드 확인
-                val responseCode = response.code()
                 if (response.isSuccessful) {
-                    // 성공적인 응답
-                    callback("로그인 성공: ${response.body()} (응답 코드: $responseCode)")
+                    val token = response.body() // 응답으로 받은 토큰
+                    callback("로그인 성공", token)
                 } else {
-                    // 실패한 응답
-                    callback("로그인 실패: ${response.message()} (응답 코드: $responseCode)")
+                    callback("로그인 실패", null)
                 }
             }
 
             override fun onFailure(call: Call<String>, t: Throwable) {
-                // 요청 실패
-                callback("로그인 요청 실패: ${t.message}")
+                callback("로그인 실패", null)
             }
         })
     }
