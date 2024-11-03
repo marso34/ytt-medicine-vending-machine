11dbd09b85bbce4b3533fd99046887da2b946f96
ylose <heungeob1003@gmail.com>
2024-11-03 18:16:59 +0900

    feat: QR 코드 라이브러리 추가 #67

--- FE/app/build.gradle.kts
+++ FE/app/build.gradle.kts
@@ -91,4 +91,10 @@ dependencies {
 
     // Security
     implementation("androidx.security:security-crypto:1.1.0-alpha03")
+
+    // QR 코드
+    implementation("com.journeyapps:zxing-android-embedded:4.3.0" )
+    implementation("com.google.zxing:core:3.3.0")
+
+
 }
