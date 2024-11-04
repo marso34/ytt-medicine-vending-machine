08266e0c38ff4047abd8cd51fb10b5d5d35e9fb7
Eeeegarden <106652275+Eeeegarden@users.noreply.github.com>
2024-11-04 16:07:25 +0900

    fix: 토큰 재발급 요청 성공 시 응답 ResponseDto 로수정

--- BE/ytt/src/main/java/com/example/ytt/domain/user/controller/AuthController.java
+++ BE/ytt/src/main/java/com/example/ytt/domain/user/controller/AuthController.java
@@ -3,6 +3,9 @@ package com.example.ytt.domain.user.controller;
 import com.example.ytt.domain.user.dto.TokenResponseDto;
 import com.example.ytt.domain.user.service.AuthService;
 import com.example.ytt.global.common.annotation.SwaggerApi;
+import com.example.ytt.global.common.response.ResponseDto;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -26,8 +29,11 @@ public class AuthController {
      */
     @PostMapping("/reissue")
     @SwaggerApi(summary = "토큰 갱신", description = "사용자의 Refresh Token을 사용하여 새로운 Access Token을 발급합니다.", implementation = TokenResponseDto.class)
+    @Parameters({
+            @Parameter(name = "refresh", description = "리프레시 토큰을 헤더에 포함하여 전송합니다.")
+    })
     public ResponseEntity<?> reissue(HttpServletRequest request, HttpServletResponse response) {
         TokenResponseDto tokenResponseDto = authService.reissueTokens(request, response);
-        return ResponseEntity.ok(tokenResponseDto);
+        return ResponseEntity.ok(ResponseDto.of(200, "토큰이 재발급 되었습니다.", tokenResponseDto));
     }
 }
