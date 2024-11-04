e15d3f5c431e8ba51e624474838e9e7dd548de5a
LEE JUNE <mars102aq@gmail.com>
2024-11-04 20:20:11 +0900

    feat: inventory 삭제 추가 #63

--- BE/ytt/src/main/java/com/example/ytt/domain/inventory/repository/InventoryRepository.java
+++ BE/ytt/src/main/java/com/example/ytt/domain/inventory/repository/InventoryRepository.java
@@ -14,4 +14,6 @@ public interface InventoryRepository extends JpaRepository<Inventory, Long> {
 
     List<Inventory> findByMedicineId(Long medicineId); // 약품 ID로 검색 (특정 약품을 가지고 있는 재고 목록 (자판기) 조회)
 
+    void deleteByVendingMachineIdAndMedicineId(Long vendingMachineId, Long medicineId); // 자판기 ID와 약품 ID로 삭제 (특정 자판기의 특정 약품 삭제)
+
 }
