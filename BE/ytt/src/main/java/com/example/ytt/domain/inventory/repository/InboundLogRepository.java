696ea77969317b208d24215734f9d58c3589f517
LEE JUNE <mars102aq@gmail.com>
2024-11-04 20:21:58 +0900

    feat: InboundLog(입고) 추가 #63

---
+++ BE/ytt/src/main/java/com/example/ytt/domain/inventory/repository/InboundLogRepository.java
@@ -0,0 +1,12 @@
+package com.example.ytt.domain.inventory.repository;
+
+import com.example.ytt.domain.inventory.domain.InboundLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface InboundLogRepository extends JpaRepository<InboundLog, Long> {
+
+    List<InboundLog> findByVendingMachineId(Long machineId);
+
+}
