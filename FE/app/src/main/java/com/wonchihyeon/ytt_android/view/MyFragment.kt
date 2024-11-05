932f558cf52da0821bf1dd0ff0c0eb8b1b26f75f
ylose <heungeob1003@gmail.com>
2024-11-05 14:28:28 +0900

    feat: 로그아웃 기능 추가  #61

--- FE/app/src/main/java/com/wonchihyeon/ytt_android/view/MyFragment.kt
+++ FE/app/src/main/java/com/wonchihyeon/ytt_android/view/MyFragment.kt
@@ -1,5 +1,6 @@
 package com.wonchihyeon.ytt_android.view
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,11 +8,12 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.wonchihyeon.ytt_android.R
+import com.wonchihyeon.ytt_android.auth.LoginActivity
+import com.wonchihyeon.ytt_android.data.TokenManager
 import com.wonchihyeon.ytt_android.databinding.FragmentMyBinding
 
 // 사용자 페이지
 class MyFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private lateinit var binding: FragmentMyBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,7 +27,20 @@ class MyFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_my, container, false)
 
+        // 로그아웃 버튼 클릭 리스너 설정
+        binding.logoutButton.setOnClickListener {
+            logout()
+        }
 
         return binding.root
+    }
+
+    private fun logout() {
+        // 토큰 삭제
+        TokenManager.clearToken(context)
+
+
+        startActivity(Intent(context, LoginActivity::class.java))
+        requireActivity().finish() // 현재 프래그먼트 종료
     }
 }
