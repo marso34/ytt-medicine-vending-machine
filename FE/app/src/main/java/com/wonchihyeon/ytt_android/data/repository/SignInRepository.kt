5ebe4bac53b8ac8bac9b857390994bd715dce9a8
ylose <heungeob1003@gmail.com>
2024-11-05 12:38:00 +0900

    feat: 회원가입 테스팅  #61

--- FE/app/src/main/java/com/wonchihyeon/ytt_android/data/repository/SignInRepository.kt
+++ FE/app/src/main/java/com/wonchihyeon/ytt_android/data/repository/SignInRepository.kt
@@ -1,6 +1,7 @@
 package com.wonchihyeon.ytt_android.data.repository
 
 import android.content.Context
+import com.wonchihyeon.ytt_android.data.model.ResponseDTO
 import com.wonchihyeon.ytt_android.data.model.SignInDTO
 import com.wonchihyeon.ytt_android.data.network.ApiService
 import com.wonchihyeon.ytt_android.data.network.RetrofitAPI
@@ -13,11 +14,14 @@ class SignInRepository(private val context: Context) {
     private val apiService: ApiService = RetrofitAPI.getRetrofit(context).create(ApiService::class.java)
 
     fun signIn(signInDTO: SignInDTO, callback: (String, String?) -> Unit) {
-        apiService.signIn(signInDTO).enqueue(object : Callback<String> {
-            override fun onResponse(call: Call<String>, response: Response<String>) {
+        apiService.signIn(signInDTO).enqueue(object : Callback<ResponseDTO<String>>  {
+            override fun onResponse(
+                call: Call<ResponseDTO<String>>,
+                response: Response<ResponseDTO<String>>
+            ) {
                 if (response.isSuccessful) {
                     response.body()?.let { token ->
-                        callback("로그인 성공", token)
+                        callback("로그인 성공", token.toString())
                     } ?: run {
                         callback("로그인 실패: 응답이 없습니다", null)
                     }
@@ -27,7 +31,7 @@ class SignInRepository(private val context: Context) {
                 }
             }
 
-            override fun onFailure(call: Call<String>, t: Throwable) {
+            override fun onFailure(call: Call<ResponseDTO<String>>, t: Throwable) {
                 callback("로그인 실패: ${t.message}", null)
             }
         })
