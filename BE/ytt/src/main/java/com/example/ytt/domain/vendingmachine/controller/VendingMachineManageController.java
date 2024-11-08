0d929fcabd5adb1053734121b665cffb7dbeb564
LEE JUNE <mars102aq@gmail.com>
2024-11-08 16:24:23 +0900

    feat: 유저 권한에 따른 예외 처리 틀 마련 #75

--- BE/ytt/src/main/java/com/example/ytt/domain/vendingmachine/controller/VendingMachineManageController.java
+++ BE/ytt/src/main/java/com/example/ytt/domain/vendingmachine/controller/VendingMachineManageController.java
@@ -1,6 +1,8 @@
 package com.example.ytt.domain.vendingmachine.controller;
 
 import com.example.ytt.domain.inventory.dto.InboundReqDto;
+import com.example.ytt.domain.user.auth.security.CustomUserDetails;
+import com.example.ytt.domain.user.dto.Role;
 import com.example.ytt.domain.vendingmachine.dto.VendingMachineDetailDto;
 import com.example.ytt.domain.vendingmachine.dto.VendingMachineReqDto;
 import com.example.ytt.domain.vendingmachine.service.VendingMachineManageService;
@@ -9,9 +11,13 @@ import com.example.ytt.global.common.response.ResponseDto;
 import com.example.ytt.global.util.ResponseUtil;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/vending-machine")
 @RequiredArgsConstructor
@@ -22,15 +28,25 @@ public class VendingMachineManageController {
 
     @PostMapping("/create")
     @SwaggerApi(summary = "자판기 생성", description = "자판기 생성 API", implementation = ResponseDto.class)
-    public ResponseEntity<ResponseDto<VendingMachineDetailDto>> createVendingMachine(@RequestBody VendingMachineReqDto reqDto) {
-        return ResponseUtil.success(vendingMachineService.createVendingMachine(reqDto));
+    public ResponseEntity<ResponseDto<VendingMachineDetailDto>> createVendingMachine(@RequestBody VendingMachineReqDto reqDto, @AuthenticationPrincipal CustomUserDetails user) {
+        if (user.getRole().equals(Role.CUSTOMER)) {
+            log.warn("WARN: {}", "자판기를 생성할 권한이 없습니다. (권한: CUSTOMER)"); // TODO: 아래로 변경 필요
+//            return ResponseUtil.error(ExceptionType.FORBIDDEN_USER, "자판기를 생성할 권한이 없습니다.", null);
+        }
+
+        return ResponseUtil.success(HttpStatus.CREATED, "자판기 생성 완료", vendingMachineService.createVendingMachine(reqDto));
     }
 
     // 자판기에 약 등록
     @PostMapping("/add-medicine")
     @SwaggerApi(summary = "자판기에 약 등록", description = "자판기에 약 등록 API", implementation = ResponseDto.class)
-    public ResponseEntity<ResponseDto<VendingMachineDetailDto>> addMedicineToVendingMachine(@RequestBody InboundReqDto reqDto) {
-        return ResponseUtil.success(vendingMachineService.addMedicineToVendingMachine(reqDto));
+    public ResponseEntity<ResponseDto<VendingMachineDetailDto>> addMedicineToVendingMachine(@RequestBody InboundReqDto reqDto, @AuthenticationPrincipal CustomUserDetails user) {
+        if (user.getRole().equals(Role.CUSTOMER)) {
+            log.warn("WARN: {}", "자판기에 약을 등록할 권한이 없습니다. (권한: CUSTOMER)"); // TODO: 아래로 변경 필요
+//            return ResponseUtil.error(ExceptionType.FORBIDDEN_USER, "자판기에 약을 등록할 권한이 없습니다.", null);
+        }
+
+        return ResponseUtil.success(HttpStatus.CREATED, "자판기 약 등록 완료" ,vendingMachineService.addMedicineToVendingMachine(reqDto));
     }
 
     // 자판기 운영 상태 변경
