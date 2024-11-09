230d0104096a5f36bddd9c9c8ae7e9da7847f439
Eeeegarden <106652275+Eeeegarden@users.noreply.github.com>
2024-11-09 16:02:28 +0900

    refactor: Dto 수정

--- BE/ytt/src/main/java/com/example/ytt/domain/order/dto/OrderDto.java
+++ BE/ytt/src/main/java/com/example/ytt/domain/order/dto/OrderDto.java
@@ -2,41 +2,39 @@ package com.example.ytt.domain.order.dto;
 
 import com.example.ytt.domain.order.domain.Order;
 import com.example.ytt.domain.order.domain.OrderState;
-import com.example.ytt.domain.user.domain.User;
-import com.example.ytt.domain.vendingmachine.domain.VendingMachine;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Schema(title = "주문 Response DTO", description = "주문 정보를 담은 DTO")
 public record OrderDto(
-        @Schema(description = "주문 ID") Long id,
-        @Schema(description = "주문자 ID") User user,
-        @Schema(description = "자판기 ID")VendingMachine vendingMachine,
-        @Schema(description = "주문 상태") OrderState orderState,
+        @Schema(description = "주문 UUID") Long id,
+        @Schema(description = "주문자 ID") Long userId,
+        @Schema(description = "주문자 이름") String userName,
+        @Schema(description = "자판기 ID")String vendingMachineName,
+        @Schema(description = "주문 상태") String orderState, //
         @Schema(description = "주문 시간") LocalDateTime orderAt,
-        @Schema(description = "완료 시간") LocalDateTime completedAt,
         @Schema(description = "총 가격") int totalPrice,
         @Schema(description = "주문 상세 목록") List<OrderDetailDto> orderItems
 ) {
-    public static OrderDto of(Long id, User user, VendingMachine vendingMachine, OrderState orderState,
-                              LocalDateTime orderAt, LocalDateTime completedAt, int totalPrice, List<OrderDetailDto> orderItems) {
-        return new OrderDto(id, user, vendingMachine, orderState, orderAt, completedAt, totalPrice, orderItems);
+    public static OrderDto of(Long id, Long userId, String userName, String vendingMachineName, OrderState orderState,
+                              LocalDateTime orderAt, int totalPrice, List<OrderDetailDto> orderItems) {
+        return new OrderDto(id, userId, userName, vendingMachineName, orderState.getMessage(), orderAt, totalPrice, orderItems);
     }
 
     public static OrderDto from(Order order) {
         return new OrderDto(
                 order.getId(),
-                order.getUser(),
-                order.getVendingMachine(),
-                order.getOrderState(),
+                order.getUser().getId(),
+                order.getUser().getName(),
+                order.getVendingMachine().getName(),
+                order.getOrderState().getMessage(),
                 order.getOrderAt(),
-                order.getCompletedAt(),
                 order.getTotalPrice(),
                 order.getOrderItems().stream()
                         .map(OrderDetailDto::from)
-                        .collect(Collectors.toList())
+                        .toList()
         );
     }
+
 }
--- BE/ytt/src/main/java/com/example/ytt/domain/order/dto/OrderReqDto.java
+++ BE/ytt/src/main/java/com/example/ytt/domain/order/dto/OrderReqDto.java
@@ -1,17 +1,12 @@
 package com.example.ytt.domain.order.dto;
 
-import com.example.ytt.domain.order.domain.OrderDetail;
-import com.example.ytt.domain.user.domain.User;
-import com.example.ytt.domain.vendingmachine.domain.VendingMachine;
 import io.swagger.v3.oas.annotations.media.Schema;
-
 import java.util.List;
 
 @Schema(title = "주문 생성 Request DTO", description = "주문 생성 요청 DTO")
 public record OrderReqDto(
-        @Schema(description = "사용자 Id")   User userId,
-        @Schema(description = "자판기 Id")   VendingMachine bendingMachineId,
-        @Schema(description = "총 가격"  )   int price,
-        @Schema(description = "상품 리스트") List<OrderDetail> orderItems
+        @Schema(description = "사용자 Id")   Long userId,
+        @Schema(description = "자판기 Id")   Long vendingMachineId,
+        @Schema(description = "상품 리스트") List<OrderDetailReqDto> orderItems
 ) {
 }
