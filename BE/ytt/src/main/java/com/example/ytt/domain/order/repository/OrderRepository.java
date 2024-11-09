b6a0165c07ddd4bb60bfab28f0af165098f830a2
Eeeegarden <106652275+Eeeegarden@users.noreply.github.com>
2024-11-09 16:00:50 +0900

    feat: id로 Order찾도록 추가

--- BE/ytt/src/main/java/com/example/ytt/domain/order/repository/OrderRepository.java
+++ BE/ytt/src/main/java/com/example/ytt/domain/order/repository/OrderRepository.java
@@ -2,7 +2,10 @@ package com.example.ytt.domain.order.repository;
 
 import com.example.ytt.domain.order.domain.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
-public interface OrderRepository extends JpaRepository<Order, Integer> {
-    Order findByUserId(int userId);
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    Order findByUserId(Long userId);
+
+    Optional<Order> findById(Long id);
 }
