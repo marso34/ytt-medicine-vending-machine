181cba19812625e1f4a4e8d11fcbbba9d4f61841
ylose <heungeob1003@gmail.com>
2024-11-10 09:26:43 +0900

    feat: 자판기 DTO 생성  #67

---
+++ FE/app/src/main/java/com/wonchihyeon/ytt_android/data/model/VendingMachineDetailDTO.kt
@@ -0,0 +1,13 @@
+package com.wonchihyeon.ytt_android.data.model
+
+data class VendingMachineDetailDTO(
+    val id: Int,
+    val name: String,
+    val state: String,
+    val address: String,
+    val latitude: Double,
+    val longtitude: Double,
+    val capacity: Int,
+    val isFavorite: Boolean,
+    val medicines: List<Medicine>,
+)
