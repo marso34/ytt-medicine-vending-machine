3f1958d723878d9c077e794ba1114d82678ca453
ylose <heungeob1003@gmail.com>
2024-11-12 16:45:16 +0900

    feat: 자판기 DTO 추가 #67

--- FE/app/src/main/java/com/wonchihyeon/ytt_android/data/model/VendingMachineDTO.kt
+++ FE/app/src/main/java/com/wonchihyeon/ytt_android/data/model/VendingMachineDTO.kt
@@ -1,24 +1,10 @@
 package com.wonchihyeon.ytt_android.data.model
 
-import com.wonchihyeon.ytt_android.MachineItem
-
-data class VendingMachineDTO (
+data class VendingMachineDTO(
     val id: Int,
     val name: String,
-    val state: state,
+    val state: String,
     val address: String,
     val latitude: Double,
-    val longitude: Double,
-)  {
-    companion object {
-        fun fromDTO(dto: VendingMachineDTO): MachineItem {
-            return MachineItem(
-                id = dto.id,
-                name = dto.name,
-                address = dto.address,
-                latitude = dto.latitude,
-                longitude = dto.longitude,
-            )
-        }
-    }
-}
+    val longitude: Double
+)
