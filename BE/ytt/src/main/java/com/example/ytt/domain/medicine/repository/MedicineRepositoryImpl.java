33395a9017416e86e01bb7e8f68343a54e42a2ce
LEE JUNE <mars102aq@gmail.com>
2024-11-14 21:27:10 +0900

    feat: 약 repository QueryDSL 적용 #85
    - 이름, 제조사, 성분을 포함한 리스트 조회
    - id or poductCode를 사용한 MedicineDetail 조회

---
+++ BE/ytt/src/main/java/com/example/ytt/domain/medicine/repository/MedicineRepositoryImpl.java
@@ -0,0 +1,72 @@
+package com.example.ytt.domain.medicine.repository;
+
+import com.example.ytt.domain.medicine.domain.Medicine;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.example.ytt.domain.medicine.domain.QIngredient.ingredient;
+import static com.example.ytt.domain.medicine.domain.QMedicine.medicine;
+import static com.example.ytt.domain.medicine.domain.QMedicineIngredient.medicineIngredient;
+
+@Repository
+@RequiredArgsConstructor
+public class MedicineRepositoryImpl implements MedicineRepositoryCustom {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public List<Medicine> getMedicines(String name, String manufacturer, Long ingredientId) {
+        return jpaQueryFactory
+                .selectFrom(medicine)
+                .leftJoin(medicine.ingredients, medicineIngredient)
+                .leftJoin(medicineIngredient.ingredient, ingredient)
+                .where(
+                        nameContains(name),
+                        manufacturerContains(manufacturer),
+                        ingredientContains(ingredientId)
+                )
+                .fetch();
+    }
+
+    @Override
+    public Optional<Medicine> getMedicineDetail(Long medicineId, String poductCode) {
+        Medicine tMedicine = jpaQueryFactory
+                .selectFrom(medicine)
+                .leftJoin(medicine.ingredients, medicineIngredient).fetchJoin()
+                .leftJoin(medicineIngredient.ingredient, ingredient).fetchJoin()
+                .where(
+                        equalsMedicineId(medicineId),
+                        equalsProductCode(poductCode)
+                )
+                .fetchOne();
+
+        return Optional.ofNullable(tMedicine);
+    }
+
+    private BooleanExpression nameContains(String name) {
+        return name != null ? medicine.name.contains(name) : null;
+    }
+
+    private BooleanExpression manufacturerContains(String manufacturer) {
+        return manufacturer != null ? medicine.manufacturer.contains(manufacturer) : null;
+    }
+
+    private BooleanExpression ingredientContains(Long ingredientId) {
+        return ingredientId != null ? medicine.ingredients.any().ingredient.id.eq(ingredientId) : null;
+    }
+
+    private BooleanExpression equalsMedicineId(Long medicineId) {
+        return medicineId != null ? medicine.id.eq(medicineId) : null;
+    }
+
+    private BooleanExpression equalsProductCode(String productCode) {
+        return productCode != null ? medicine.productCode.eq(productCode) : null;
+    }
+
+
+}
