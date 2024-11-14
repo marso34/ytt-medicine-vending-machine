ad27f1aa425da7d3de762c7fdeaf436f34de4aa7
ylose <heungeob1003@gmail.com>
2024-11-14 16:01:08 +0900

    feat: 약 자판기 레포지토리 문제 해결 #83

--- FE/app/src/main/java/com/wonchihyeon/ytt_android/data/repository/VendingMachineRepository.kt
+++ FE/app/src/main/java/com/wonchihyeon/ytt_android/data/repository/VendingMachineRepository.kt
@@ -3,6 +3,7 @@ package com.wonchihyeon.ytt_android.data.repository
 import android.util.Log
 import com.wonchihyeon.ytt_android.data.model.ResponseDTO
 import com.wonchihyeon.ytt_android.data.model.VendingMachineDTO
+import com.wonchihyeon.ytt_android.data.model.vendingmachine.MedicineDTO
 import com.wonchihyeon.ytt_android.data.network.ApiService
 import retrofit2.Call
 import retrofit2.Callback
@@ -10,25 +11,41 @@ import retrofit2.Response
 
 class VendingMachineRepository(private val apiService: ApiService) {
 
-    fun getNearByMachine(latitude: Double, longitude: Double, callback: (ResponseDTO<List<VendingMachineDTO>>?) -> Unit) {
-        apiService.getNearByMachine(latitude, longitude).enqueue(object : Callback<ResponseDTO<List<VendingMachineDTO>>> {
-            override fun onResponse(
-                call: Call<ResponseDTO<List<VendingMachineDTO>>>,
-                response: Response<ResponseDTO<List<VendingMachineDTO>>>
-            ) {
-                if (response.isSuccessful) {
-                    Log.d("Response Success", response.code().toString())
-                    callback(response.body())
-                } else {
-                    Log.d("Response Error", response.code().toString())
+
+    fun getVendingMachineById(
+        vendingMachineId: String,
+        medicineId: Long,
+    ): Call<ResponseDTO<MedicineDTO>> {
+        return apiService.getVendingMachineById(vendingMachineId, medicineId)
+    }
+
+    fun getNearByMachine(
+        latitude: Double,
+        longitude: Double,
+        callback: (ResponseDTO<List<VendingMachineDTO>>?) -> Unit,
+    ) {
+        apiService.getNearByMachine(latitude, longitude)
+            .enqueue(object : Callback<ResponseDTO<List<VendingMachineDTO>>> {
+                override fun onResponse(
+                    call: Call<ResponseDTO<List<VendingMachineDTO>>>,
+                    response: Response<ResponseDTO<List<VendingMachineDTO>>>,
+                ) {
+                    if (response.isSuccessful) {
+                        Log.d("Response Success", response.code().toString())
+                        callback(response.body())
+                    } else {
+                        Log.d("Response Error", response.code().toString())
+                        callback(null)
+                    }
+                }
+
+                override fun onFailure(
+                    call: Call<ResponseDTO<List<VendingMachineDTO>>>,
+                    t: Throwable,
+                ) {
+                    Log.d("Network Failure", t.message ?: "Unknown error")
                     callback(null)
                 }
-            }
-
-            override fun onFailure(call: Call<ResponseDTO<List<VendingMachineDTO>>>, t: Throwable) {
-                Log.d("Network Failure", t.message ?: "Unknown error")
-                callback(null)
-            }
-        })
+            })
     }
 }
