f0d7678318adb5d9126aead04ddf57154ddc4879
Eeeegarden <106652275+Eeeegarden@users.noreply.github.com>
2024-11-20 05:06:42 +0900

    feat: Dto 수정

--- BE/ytt/src/main/java/com/example/ytt/domain/order/dto/OrderDetailReqDto.java
+++ BE/ytt/src/main/java/com/example/ytt/domain/order/dto/OrderDetailReqDto.java
@@ -2,9 +2,13 @@ package com.example.ytt.domain.order.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-@Schema(title = "주문 상세 Request DTO", description = "주문 상세 요청 DTO")
+@Schema(title = "주문 상세 DTO", description = "주문 상세 DTO")
 public record OrderDetailReqDto(
         @Schema(description = "약 번호") String productCode,
         @Schema(description = "수량") int quantity
 ) {
+    public static OrderDetailReqDto of(String productCode, int quantity) {
+        return new OrderDetailReqDto(productCode, quantity);
+    }
+
 }
