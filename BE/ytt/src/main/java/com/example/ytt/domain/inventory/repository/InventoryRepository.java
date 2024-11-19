31b66d138fd57de965e7335fe1861f2a70e85121
Eeeegarden <106652275+Eeeegarden@users.noreply.github.com>
2024-11-20 05:07:03 +0900

    feat: Repository 수정

--- BE/ytt/src/main/java/com/example/ytt/domain/inventory/repository/InventoryRepository.java
+++ BE/ytt/src/main/java/com/example/ytt/domain/inventory/repository/InventoryRepository.java
@@ -8,7 +8,6 @@ import java.util.Optional;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
 
-    Optional<Inventory> findByMedicineIdAndVendingMachineId(Long medicineId, Long vendingMachineId); // 약품 ID와 자판기 ID로 검색 (특정 자판기에 특정 약품)
 
     List<Inventory> findByVendingMachineId(Long vendingMachineId); // 자판기 ID로 검색 (특정 자판기의 재고 목록 조회)
 
@@ -16,4 +15,5 @@ public interface InventoryRepository extends JpaRepository<Inventory, Long> {
 
     void deleteByVendingMachineIdAndMedicineId(Long vendingMachineId, Long medicineId); // 자판기 ID와 약품 ID로 삭제 (특정 자판기의 특정 약품 삭제)
 
+    Optional<Inventory> findByMedicineIdAndVendingMachineId(Long medicineId, Long vendingMachineId); // 약품 ID와 자판기 ID로 검색 (특정 자판기에 특정 약품)
 }
