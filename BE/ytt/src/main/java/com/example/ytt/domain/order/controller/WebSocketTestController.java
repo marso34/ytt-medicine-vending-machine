e9498769b22947146e68daa285a77bf61912cadb
Eeeegarden <106652275+Eeeegarden@users.noreply.github.com>
2024-11-20 05:10:30 +0900

    test: 소켓 통신 테스트용 페이지 추가

--- BE/ytt/src/main/java/com/example/ytt/domain/order/controller/WebSocketTestController.java
+++ BE/ytt/src/main/java/com/example/ytt/domain/order/controller/WebSocketTestController.java
@@ -7,8 +7,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WebSocketTestController {
 
-    @GetMapping("/websocket-test")
-    public String testPage(Model model) {
-        return "websocket-test";
+    @GetMapping("/websocket-vm-test")
+    public String testPage1(Model model) {
+        return "websocket-vm-test";
     }
+
+    @GetMapping("/websocket-user-test")
+    public String testPage2(Model model) {
+        return "websocket-user-test";
+    }
+
 }
