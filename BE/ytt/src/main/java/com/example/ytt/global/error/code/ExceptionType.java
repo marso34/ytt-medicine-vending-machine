0e42424f88b921e69d636acc61a5229740830685
Eeeegarden <106652275+Eeeegarden@users.noreply.github.com>
2024-11-20 05:09:36 +0900

    feat: 예외처리 추가

--- BE/ytt/src/main/java/com/example/ytt/global/error/code/ExceptionType.java
+++ BE/ytt/src/main/java/com/example/ytt/global/error/code/ExceptionType.java
@@ -67,7 +67,16 @@ public enum ExceptionType {
     UNREGISTERED_INBOUND(1002, HttpStatus.BAD_REQUEST, "등록되지 않은 약의 입고는 불가합니다."), // 400 Bad Request
     NOT_FOUND_INVENOTRY(1010, HttpStatus.NOT_FOUND, "재고 정보가 없습니다."), // 404 Not Found
     NO_CONTENT_INVENOTRY(1011, HttpStatus.NO_CONTENT, "재고 정보가 없습니다."), // 204 No Content
-
+    
+    // 주문
+    NOT_FOUND_ORDER(1100, HttpStatus.NOT_FOUND, "주문을 찾을 수 없습니다."),
+    INVALID_ORDER_STATE_PENDING(1102, HttpStatus.BAD_REQUEST, "대기 중인 주문만 처리할 수 있습니다.."),
+    INVALID_ORDER_STATE_STORED(1103, HttpStatus.BAD_REQUEST, "보관 중인 주문만 처리할 수 있습니다."),
+    INSUFFICIENT_INVENTORY(1104, HttpStatus.BAD_REQUEST, "재고가 부족합니다."),
+    ORDER_STORE_SUCCESS(200, HttpStatus.OK, "보관 요청이 성공적으로 처리되었습니다."),
+    ORDER_COMPLETE_SUCCESS(200, HttpStatus.OK, "수령 완료 처리가 성공적으로 처리되었습니다."),
+    ORDER_CANCEL_SUCCESS(200, HttpStatus.OK, "주문 취소가 성공적으로 처리되었습니다."),
+    WEBSOCKET_SERVER_ERROR(500, HttpStatus.INTERNAL_SERVER_ERROR, "처리 중 오류가 발생했습니다."),
 
     // Json 형식 오류
     JSON_FORMAT_INVALID(3000, HttpStatus.BAD_REQUEST, "JSON 형식이 올바르지 않습니다."), // 400 Bad Request
