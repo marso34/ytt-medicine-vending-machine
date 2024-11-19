40bfdbb00c2a4bcc650f7db627f0f57966330508
ylose <heungeob1003@gmail.com>
2024-11-19 09:26:23 +0900

    Remove:이미지 관련 라이브러리 추가 (glide) #83

--- FE/app/build.gradle.kts
+++ FE/app/build.gradle.kts
@@ -101,5 +101,8 @@ dependencies {
 
     implementation("com.android.support:multidex:1.0.3")
 
+    implementation("com.google.android.gms:play-services-location:19.0.1")
+
+    implementation("com.github.bumptech.glide:glide:4.12.0")
 
 }
