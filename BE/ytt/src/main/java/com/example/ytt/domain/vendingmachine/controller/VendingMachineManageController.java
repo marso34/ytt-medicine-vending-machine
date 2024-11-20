623e1bdbbea755b603857b5a97e4a2195b457eb0
LEE JUNE <mars102aq@gmail.com>
2024-11-20 17:38:01 +0900

    feat: 자판기 관리 기능 추가 #91
    - 자판기 운영 상태 변경 기능
    - 즐겨찾기 추가 삭제 기능
    - inventoryRepository -> inventoryService를 사용하도록 수정

--- BE/ytt/src/main/java/com/example/ytt/domain/vendingmachine/controller/VendingMachineManageController.java
+++ BE/ytt/src/main/java/com/example/ytt/domain/vendingmachine/controller/VendingMachineManageController.java
@@ -3,11 +3,14 @@ package com.example.ytt.domain.vendingmachine.controller;
 import com.example.ytt.domain.inventory.dto.InboundReqDto;
 import com.example.ytt.domain.user.auth.security.CustomUserDetails;
 import com.example.ytt.domain.user.dto.Role;
+import com.example.ytt.domain.vendingmachine.domain.MachineState;
 import com.example.ytt.domain.vendingmachine.dto.VendingMachineDetailDto;
 import com.example.ytt.domain.vendingmachine.dto.VendingMachineReqDto;
+import com.example.ytt.domain.vendingmachine.service.FavoriteService;
 import com.example.ytt.domain.vendingmachine.service.VendingMachineManageService;
 import com.example.ytt.global.common.annotation.SwaggerApi;
 import com.example.ytt.global.common.response.ResponseDto;
+import com.example.ytt.global.error.code.ExceptionType;
 import com.example.ytt.global.util.ResponseUtil;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -21,37 +24,70 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/vending-machine")
 @RequiredArgsConstructor
-@Tag(name = "자판기 기본", description = "자판기 생성, 수정, 삭제 API")
+@Tag(name = "자판기 관리", description = "자판기 생성, 수정, 삭제 API")
 public class VendingMachineManageController {
 
     private final VendingMachineManageService vendingMachineService;
+    private final FavoriteService favoriteService;
 
-    @PostMapping("/create")
-    @SwaggerApi(summary = "자판기 생성", description = "자판기 생성 API", implementation = ResponseDto.class)
+    @PostMapping
+    @SwaggerApi(summary = "자판기 생성", description = "자판기 생성 API", responseCode = "201")
     public ResponseEntity<ResponseDto<VendingMachineDetailDto>> createVendingMachine(@RequestBody VendingMachineReqDto reqDto, @AuthenticationPrincipal CustomUserDetails user) {
         if (user.getRole().equals(Role.CUSTOMER)) {
-            log.warn("WARN: {}", "자판기를 생성할 권한이 없습니다. (권한: CUSTOMER)"); // TODO: 아래로 변경 필요
-//            return ResponseUtil.error(ExceptionType.FORBIDDEN_USER, "자판기를 생성할 권한이 없습니다.", null);
+            return ResponseUtil.error(ExceptionType.FORBIDDEN_USER, "자판기를 생성할 권한이 없습니다.", null);
         }
 
-        return ResponseUtil.success(HttpStatus.CREATED, "자판기 생성 완료", vendingMachineService.createVendingMachine(reqDto));
+        return ResponseUtil.success(HttpStatus.CREATED, "자판기 생성 완료", vendingMachineService.createVendingMachine(reqDto, user.getId()));
     }
 
-    // 자판기에 약 등록
-    @PostMapping("/add-medicine")
-    @SwaggerApi(summary = "자판기에 약 등록", description = "자판기에 약 등록 API", implementation = ResponseDto.class)
-    public ResponseEntity<ResponseDto<VendingMachineDetailDto>> addMedicineToVendingMachine(@RequestBody InboundReqDto reqDto, @AuthenticationPrincipal CustomUserDetails user) {
+    @PatchMapping("/{machineId}")
+    @SwaggerApi(summary = "자판기 운영 상태 변경", description = "자판기 운영 상태 변경 API")
+    public ResponseEntity<ResponseDto<VendingMachineDetailDto>> openVendingMachine(
+            @PathVariable(value = "machineId") Long machineId,
+            @AuthenticationPrincipal CustomUserDetails user
+    ) {
         if (user.getRole().equals(Role.CUSTOMER)) {
-            log.warn("WARN: {}", "자판기에 약을 등록할 권한이 없습니다. (권한: CUSTOMER)"); // TODO: 아래로 변경 필요
-//            return ResponseUtil.error(ExceptionType.FORBIDDEN_USER, "자판기에 약을 등록할 권한이 없습니다.", null);
+            return ResponseUtil.error(ExceptionType.FORBIDDEN_USER, "자판기 운영 상태를 변경할 권한이 없습니다.", null);
         }
 
-        return ResponseUtil.success(HttpStatus.CREATED, "자판기 약 등록 완료" ,vendingMachineService.addMedicineToVendingMachine(reqDto));
+        return ResponseUtil.success(vendingMachineService.openVendingMachine(machineId, MachineState.OPERATING));
     }
 
-    // 자판기 운영 상태 변경
+    // TODO: 자판기 삭제
+//    @DeleteMapping("/{machineId}")
 
-    // 자판기 약 삭제
+    @PostMapping("/{machineId}/medicine")
+    @SwaggerApi(summary = "자판기에 약 등록", description = "자판기에 약 등록 API")
+    public ResponseEntity<ResponseDto<VendingMachineDetailDto>> addMedicineToVendingMachine(
+            @PathVariable(value = "machineId") Long machineId,
+            @RequestBody InboundReqDto reqDto,
+            @AuthenticationPrincipal CustomUserDetails user
+    ) {
+        if (user.getRole().equals(Role.CUSTOMER)) {
+            return ResponseUtil.error(ExceptionType.FORBIDDEN_USER, "자판기에 약을 등록할 권한이 없습니다.", null);
+        }
 
-    // 자판기 삭제
+        return ResponseUtil.success(HttpStatus.CREATED, "자판기 약 등록 완료" ,vendingMachineService.addMedicineToVendingMachine(machineId, reqDto, user.getId()));
+    }
+
+    // TODO: 자판기 약 수정
+//    @PatchMapping("/{machineId}/medicine/{medicineId}")
+
+    // TODO: 자판기 약 삭제
+//    @DeleteMapping("/{machineId}/medicine/{medicineId}")
+
+    @PostMapping("/favorites/{machineId}")
+    @SwaggerApi(summary = "즐겨찾기 추가", description = "자판기를 즐겨찾기에 추가하는 API")
+    public ResponseEntity<ResponseDto<Boolean>> addFavorite(@PathVariable(value = "machineId") Long productId, @AuthenticationPrincipal CustomUserDetails user) {
+        favoriteService.addFavorite(user.getId(), productId);
+        return ResponseUtil.success(null);
+    }
+
+    @DeleteMapping("/favorites/{machineId}")
+    @SwaggerApi(summary = "즐겨찾기 삭제", description = "자판기를 즐겨찾기에서 삭제하는 API")
+    public ResponseEntity<ResponseDto<Boolean>> removeFavorite(@PathVariable(value = "machineId") Long productId, @AuthenticationPrincipal CustomUserDetails user) {
+        boolean isRemoved = favoriteService.removeFavorite(user.getId(), productId);
+        return ResponseUtil.success(isRemoved);
+    }
+
 }
--- BE/ytt/src/main/java/com/example/ytt/domain/vendingmachine/service/VendingMachineManageService.java
+++ BE/ytt/src/main/java/com/example/ytt/domain/vendingmachine/service/VendingMachineManageService.java
@@ -2,10 +2,13 @@ package com.example.ytt.domain.vendingmachine.service;
 
 import com.example.ytt.domain.inventory.domain.Inventory;
 import com.example.ytt.domain.inventory.dto.InboundReqDto;
-import com.example.ytt.domain.inventory.repository.InventoryRepository;
+import com.example.ytt.domain.inventory.service.InventoryService;
 import com.example.ytt.domain.medicine.domain.Medicine;
 import com.example.ytt.domain.medicine.exception.MedicineException;
 import com.example.ytt.domain.medicine.repository.MedicineRepository;
+import com.example.ytt.domain.user.domain.User;
+import com.example.ytt.domain.user.exception.UserException;
+import com.example.ytt.domain.user.repository.UserRepository;
 import com.example.ytt.domain.vendingmachine.domain.MachineState;
 import com.example.ytt.domain.vendingmachine.domain.VendingMachine;
 import com.example.ytt.domain.vendingmachine.dto.VendingMachineDetailDto;
@@ -23,30 +26,32 @@ import org.springframework.stereotype.Service;
 public class VendingMachineManageService {
 
     private final VendingMachineRepository vendingMachineRepository;
-    private final InventoryRepository inventoryRepository;
+    private final UserRepository userRepository;
     private final MedicineRepository medicineRepository;
 
-    public VendingMachineDetailDto createVendingMachine(VendingMachineReqDto reqDto) {
+    private final InventoryService inventoryService;
+
+    // 자판기 생성
+    public VendingMachineDetailDto createVendingMachine(VendingMachineReqDto reqDto, Long userId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new UserException(ExceptionType.NOT_FOUND_USER));
         VendingMachine saved = vendingMachineRepository.save(VendingMachine.from(reqDto));
 
         return VendingMachineDetailDto.from(saved);
     }
 
-    // 자판기 운영 상태로 변경
-    public VendingMachineDetailDto openVendingMachine(Long vendingMachineId) {
+    // 자판기 운영 상태 변경
+    public VendingMachineDetailDto openVendingMachine(Long vendingMachineId, MachineState state) {
         VendingMachine vendingMachine = vendingMachineRepository.findById(vendingMachineId).orElseThrow(() -> new VendingMachineException(ExceptionType.NOT_FOUND_VENDING_MACHINE));
-        vendingMachine.updateState(MachineState.OPERATING);
-
-        VendingMachine saved = vendingMachineRepository.save(vendingMachine);
+        VendingMachine saved = vendingMachineRepository.save(vendingMachine.updateState(state));
 
         return VendingMachineDetailDto.from(saved);
     }
 
     // 자판기에 약 등록
-    public VendingMachineDetailDto addMedicineToVendingMachine(InboundReqDto reqDto) {
-        VendingMachine vendingMachine = vendingMachineRepository.findById(reqDto.machineId()).orElseThrow(() -> new VendingMachineException(ExceptionType.NOT_FOUND_VENDING_MACHINE));
+    public VendingMachineDetailDto addMedicineToVendingMachine(Long machineId, InboundReqDto reqDto, Long userId) {
+        VendingMachine vendingMachine = vendingMachineRepository.findById(machineId).orElseThrow(() -> new VendingMachineException(ExceptionType.NOT_FOUND_VENDING_MACHINE));
         Medicine medicine = medicineRepository.findById(reqDto.medicineId()).orElseThrow(() -> new MedicineException(ExceptionType.NOT_FOUND_MEDICINE));
-        Inventory inventory = inventoryRepository.save(Inventory.of(vendingMachine, medicine, reqDto.quantity()));
+        Inventory inventory = inventoryService.save(vendingMachine, medicine, reqDto.quantity());
 
         vendingMachine.addInventory(inventory);
 
@@ -55,7 +60,7 @@ public class VendingMachineManageService {
 
     // 자판기에서 약 삭제
     public void deleteMedicineFromVendingMachine(Long machineId, Long medicineId) {
-        inventoryRepository.deleteByVendingMachineIdAndMedicineId(machineId, medicineId);
+        inventoryService.delete(machineId, medicineId);
     }
 
     // 자판기 삭제
