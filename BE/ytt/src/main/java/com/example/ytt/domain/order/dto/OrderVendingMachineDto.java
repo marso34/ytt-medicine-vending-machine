5e2c52e9acd4bbdc99efb0599cd28cb7e4babb79
LEE JUNE <mars102aq@gmail.com>
2024-11-28 19:14:28 +0900

    fix: OrderDetailDto 클래스명 오타 수정

--- BE/ytt/src/main/java/com/example/ytt/domain/order/dto/OrderVendingMachineDto.java
+++ BE/ytt/src/main/java/com/example/ytt/domain/order/dto/OrderVendingMachineDto.java
@@ -13,7 +13,7 @@ public record OrderVendingMachineDto(
         @Schema(description = "유저 ID")      Long userId,
         @Schema(description = "주문 상태")     OrderState orderState,
         @Schema(description = "주문 시간")     LocalDateTime orderAt,
-        @Schema(description = "주문 상세 목록") List<OrderItemReqDto> orderItems
+        @Schema(description = "주문 상세 목록") List<OrderDetailReqDto> orderItems
 ){
     public static OrderVendingMachineDto of(UUID id, Long userId, OrderState orderState, LocalDateTime orderAt, List<OrderDetailReqDto> orderItems) {
         return new OrderVendingMachineDto(id.toString(), userId, orderState, orderAt, orderItems);
