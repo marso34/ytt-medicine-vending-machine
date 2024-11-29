415a246b7214e01cdfbe18a18ed7d02d16b96c77
LEE JUNE <mars102aq@gmail.com>
2024-11-29 22:40:32 +0900

    feat: 주문 예외 처리 추가 #98

--- BE/ytt/src/main/java/com/example/ytt/global/error/code/ExceptionType.java
+++ BE/ytt/src/main/java/com/example/ytt/global/error/code/ExceptionType.java
@@ -74,12 +74,16 @@ public enum ExceptionType {
     
     // 주문
     NOT_FOUND_ORDER(1100, HttpStatus.NOT_FOUND, "주문을 찾을 수 없습니다."),
+    NO_CONTENT_ORDER(1101, HttpStatus.NO_CONTENT, "주문 정보가 없습니다."),
     INVALID_ORDER_STATE_PENDING(1102, HttpStatus.BAD_REQUEST, "대기 중인 주문만 처리할 수 있습니다.."),
     INVALID_ORDER_STATE_STORED(1103, HttpStatus.BAD_REQUEST, "보관 중인 주문만 처리할 수 있습니다."),
     INSUFFICIENT_INVENTORY(1104, HttpStatus.BAD_REQUEST, "재고가 부족합니다."),
+    FULL_VENDING_MACHINE_ORDER(1105, HttpStatus.BAD_REQUEST, "자판기 보관함이 가득 찼습니다."),
+
     ORDER_STORE_SUCCESS(200, HttpStatus.OK, "보관 요청이 성공적으로 처리되었습니다."),
     ORDER_COMPLETE_SUCCESS(200, HttpStatus.OK, "수령 완료 처리가 성공적으로 처리되었습니다."),
     ORDER_CANCEL_SUCCESS(200, HttpStatus.OK, "주문 취소가 성공적으로 처리되었습니다."),
+
     WEBSOCKET_SERVER_ERROR(500, HttpStatus.INTERNAL_SERVER_ERROR, "처리 중 오류가 발생했습니다."),
 
     // Json 형식 오류
