661a1b716c15f19aa2e2dd9435f3a75f050d86a2
LEE JUNE <mars102aq@gmail.com>
2024-11-30 23:32:52 +0900

    refactor: enum 클래스 대소문자 상관없이 수

--- BE/ytt/src/main/java/com/example/ytt/domain/order/domain/OrderState.java
+++ BE/ytt/src/main/java/com/example/ytt/domain/order/domain/OrderState.java
@@ -6,10 +6,10 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum OrderState {
-    PENDING("주문 대기 중", "잠시만 기다려주세요."),
-    STORED("보관중", "자판기에서 약을 가져가세요"),
-    COMPLETED("주문 완료", "주문이 완료되었습니다"),
-    CANCELED("주문 실패","주문이 취소됐어요. 다음에 다시 주문해주세요"),
+    PENDING("pending", "잠시만 기다려주세요."),
+    STORED("stored", "자판기에서 약을 가져가세요"),
+    COMPLETED("completed", "주문이 완료되었습니다"),
+    CANCELED("canceled","주문이 취소됐어요. 다음에 다시 주문해주세요"),
     UNKNOWN("unknown", "알 수 없는 오류가 발생했습니다.");
     
     private final String state;
@@ -17,7 +17,7 @@ public enum OrderState {
 
     public static OrderState from(String state) {
         for (OrderState orderState : OrderState.values()) {
-            if (orderState.state.equals(state)) {
+            if (orderState.state.equalsIgnoreCase(state)) {
                 return orderState;
             }
         }
