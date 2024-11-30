6e8c9376024bbb62573511f5211384df9c5c02fb
LEE JUNE <mars102aq@gmail.com>
2024-11-30 23:54:12 +0900

    feat: OrderRepository QueryDSL 적용 #98
    - 유저, 자판기, 주문 상태에 따른 주문 리스트 조회 기능 추가
    - 주문 상세 조회 기능 추가 (fetchJoin 적용)
    - 주문 수 조회
    - 주문 유저 확인 기능 추가

--- BE/ytt/src/main/java/com/example/ytt/domain/order/repository/OrderRepository.java
+++ BE/ytt/src/main/java/com/example/ytt/domain/order/repository/OrderRepository.java
@@ -2,14 +2,10 @@ package com.example.ytt.domain.order.repository;
 
 import com.example.ytt.domain.order.domain.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
+
 import java.util.Optional;
 import java.util.UUID;
 
-public interface OrderRepository extends JpaRepository<Order, UUID> {
-    List<Order> findByUserId(Long userId);
-
-    List<Order> findByVendingMachineId(Long vendingMacingId);
-
+public interface OrderRepository extends JpaRepository<Order, UUID>, OrderRepositoryCustom {
     Optional<Order> findById(UUID id);
 }
---
+++ BE/ytt/src/main/java/com/example/ytt/domain/order/repository/OrderRepositoryImpl.java
@@ -0,0 +1,87 @@
+package com.example.ytt.domain.order.repository;
+
+import com.example.ytt.domain.order.domain.Order;
+import com.example.ytt.domain.order.domain.OrderState;
+import com.example.ytt.domain.order.util.UUIDutil;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.example.ytt.domain.medicine.domain.QMedicine.medicine;
+import static com.example.ytt.domain.order.domain.QOrder.order;
+import static com.example.ytt.domain.order.domain.QOrderItem.orderItem;
+import static com.example.ytt.domain.vendingmachine.domain.QVendingMachine.vendingMachine;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryImpl implements OrderRepositoryCustom {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public boolean existsByIDAndUserId(String id, Long userId) {
+        Long count = jpaQueryFactory
+                .select(order.id.count())
+                .from(order)
+                .where(
+                        order.id.eq(UUIDutil.convertUUID(id)),
+                        eqUser(userId))
+                .fetchOne();
+
+        return count != null && count > 0;
+    }
+
+    @Override
+    public List<Order> getOrders(Long userId, Long machineId, OrderState state) {
+        return jpaQueryFactory
+                .selectFrom(order)
+                .join(order.vendingMachine, vendingMachine).fetchJoin()
+                .where(
+                        eqUser(userId),
+                        eqVendingMachine(machineId),
+                        eqState(state))
+                .fetch();
+    }
+
+    @Override
+    public Optional<Order> getOrderDetail(String orderId) {
+        Order orders = jpaQueryFactory
+                .selectFrom(order)
+                .join(order.vendingMachine, vendingMachine).fetchJoin()
+                .join(order.orderItems, orderItem).fetchJoin()
+                .join(orderItem.medicine, medicine).fetchJoin()
+                .where(
+                        order.id.eq(UUIDutil.convertUUID(orderId)))
+                .fetchOne();
+
+        return Optional.ofNullable(orders);
+    }
+
+    @Override
+    public Long getOrderCount(Long machineId, OrderState state) {
+        return jpaQueryFactory
+                .select(order.id.count())
+                .from(order)
+                .where(
+                        eqVendingMachine(machineId),
+                        eqState(state))
+                .fetchOne();
+    }
+
+    private BooleanExpression eqUser(Long userId) {
+        return userId != null ? order.user.id.eq(userId) : null;
+    }
+
+    private BooleanExpression eqVendingMachine(Long machineId) {
+        return machineId != null ? order.vendingMachine.id.eq(machineId) : null;
+    }
+
+    private BooleanExpression eqState(OrderState state) {
+        return state != null ? order.orderState.eq(state) : null;
+    }
+
+}
