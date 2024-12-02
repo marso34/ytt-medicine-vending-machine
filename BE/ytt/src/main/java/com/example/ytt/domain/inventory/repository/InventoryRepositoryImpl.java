1a9b28777605a68ce2a39299694d8f5829272d96
LEE JUNE <mars102aq@gmail.com>
2024-12-02 14:58:06 +0900

    feat: getInventories의 orderBy 메서드 추가 #98
    H2 DB 사용 시 ORDER BY FIELD를 사용할 수 없어 CaseBuilder를 사용한 메서드 추

--- BE/ytt/src/main/java/com/example/ytt/domain/inventory/repository/InventoryRepositoryImpl.java
+++ BE/ytt/src/main/java/com/example/ytt/domain/inventory/repository/InventoryRepositoryImpl.java
@@ -3,7 +3,9 @@ package com.example.ytt.domain.inventory.repository;
 import com.example.ytt.domain.inventory.domain.Inventory;
 import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.core.types.dsl.NumberExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.LockModeType;
 import lombok.RequiredArgsConstructor;
@@ -83,6 +85,10 @@ public class InventoryRepositoryImpl implements InventoryRepositoryCustom {
     }
 
     private OrderSpecifier<?> orderByFieldList(List<String> productCodes) {
+        if (productCodes == null || productCodes.isEmpty()) {
+            return null;
+        }
+
         StringBuilder template = new StringBuilder("FIELD({0}");
 
         List<Object> list = new ArrayList<>();
@@ -96,6 +102,22 @@ public class InventoryRepositoryImpl implements InventoryRepositoryCustom {
         template.append(")");
 
         return Expressions.stringTemplate(template.toString(), list).asc();
+    }
+
+    private OrderSpecifier<?> orderByFieldListForH2(List<String> productCodes) {
+        if (productCodes == null || productCodes.isEmpty()) {
+            return null;
+        }
+
+        CaseBuilder.Cases<Integer, NumberExpression<Integer>> caseBuilder = new CaseBuilder().when(medicine.productCode.eq(productCodes.get(0))).then(1);
+
+        for (int i = 1; i < productCodes.size(); i++) {
+            caseBuilder.when(medicine.productCode.eq(productCodes.get(i))).then(i+1);
+        }
+
+        NumberExpression<Integer> caseOrder = caseBuilder.otherwise(productCodes.size() + 1);
+
+        return caseOrder.asc();
     }
 
 }
