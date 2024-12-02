c21e2be9c0cbccd4c2d0fc5f22f1bc2f894323b0
Eeeegarden <106652275+Eeeegarden@users.noreply.github.com>
2024-12-02 22:48:14 +0900

    feat: 스케줄러로 기간 만료된 RefreshToken 삭제 구현 #104

--- BE/ytt/src/main/java/com/example/ytt/domain/user/repository/RefreshRepository.java
+++ BE/ytt/src/main/java/com/example/ytt/domain/user/repository/RefreshRepository.java
@@ -5,11 +5,15 @@ import com.example.ytt.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.Date;
+import java.util.List;
 
 @Repository
 public interface RefreshRepository extends JpaRepository<JwtRefresh, Long> {
 
     Boolean existsByRefresh(String refresh);
+
+    List<JwtRefresh> findAllByExpirationBefore(Date date);
 
     @Transactional
     void deleteByRefresh(String refresh);
