1048290cad0baf77a4917b3018a948e160964fc3
ylose <heungeob1003@gmail.com>
2024-12-04 18:12:36 +0900

    Feat: 자판기 저장소  #89

--- FE/app/src/main/java/com/wonchihyeon/ytt_android/data/repository/VendingMachineRepository.kt
+++ FE/app/src/main/java/com/wonchihyeon/ytt_android/data/repository/VendingMachineRepository.kt
@@ -3,7 +3,6 @@ package com.wonchihyeon.ytt_android.data.repository
 import android.util.Log
 import com.google.gson.Gson
 import com.wonchihyeon.ytt_android.data.model.ResponseDTO
-import com.wonchihyeon.ytt_android.data.model.vendingmachine.MedicineDTO
 import com.wonchihyeon.ytt_android.data.network.ApiService
 import retrofit2.Call
 import retrofit2.Callback
@@ -11,23 +10,22 @@ import retrofit2.Response
 
 class VendingMachineRepository(private val apiService: ApiService) {
 
-    fun getVendingMachineById(
-        vendingMachineId: String,
-    ): Call<ResponseDTO> {
+    fun createOrder(): Call<ResponseDTO> {
+        return apiService.createOrder()
+    }
+
+    fun myPage(): Call<ResponseDTO> {
+        return apiService.createOrder()
+    }
+
+    fun getVendingMachineById(vendingMachineId: String): Call<ResponseDTO> {
         return apiService.getVendingMachineById(vendingMachineId)
     }
 
-    fun getNearByMachine(
-        latitude: Double,
-        longitude: Double,
-        callback: (ResponseDTO) -> Unit,
-    ) {
+    fun getNearByMachine(latitude: Double, longitude: Double, callback: (ResponseDTO) -> Unit) {
         apiService.getNearByMachine(latitude, longitude)
             .enqueue(object : Callback<ResponseDTO> {
-                override fun onResponse(
-                    call: Call<ResponseDTO>,
-                    response: Response<ResponseDTO>,
-                ) {
+                override fun onResponse(call: Call<ResponseDTO>, response: Response<ResponseDTO>) {
                     if (response.isSuccessful) {
                         Log.d("Response Success", response.code().toString())
                         response.body()?.let { callback(it) }
@@ -42,41 +40,10 @@ class VendingMachineRepository(private val apiService: ApiService) {
                     }
                 }
 
-                override fun onFailure(
-                    call: Call<ResponseDTO>,
-                    t: Throwable,
-                ) {
+                override fun onFailure(call: Call<ResponseDTO>, t: Throwable) {
                     Log.d("Network Failure", t.message ?: "Unknown error")
-                    callback(ResponseDTO("400", "${t.message}", "error"))
+                    callback(ResponseDTO(400, "${t.message}", "error"))
                 }
             })
     }
-
-    fun getMedicineById(
-        medicineId: Long,
-        callback: (ResponseDTO) -> Unit
-    ) {
-        apiService.getMedicineById(medicineId).enqueue(object : Callback<ResponseDTO> {
-            override fun onResponse(call: Call<ResponseDTO>, response: Response<ResponseDTO>) {
-                if (response.isSuccessful) {
-                    Log.d("Get Medicine Success", response.code().toString())
-                    response.body()?.let { callback(it) }
-                } else {
-                    Log.d("Get Medicine Error", response.code().toString())
-                    val errorBody = response.errorBody()?.string()
-                    errorBody?.let {
-                        val gson = Gson()
-                        val errorResponse = gson.fromJson(it, ResponseDTO::class.java)
-                        callback(errorResponse)
-                    }
-                }
-            }
-
-            override fun onFailure(call: Call<ResponseDTO>, t: Throwable) {
-                Log.d("Network Failure", t.message ?: "Unknown error")
-                callback(ResponseDTO("400", "${t.message}", "error"))
-            }
-        })
-    }
-
 }
