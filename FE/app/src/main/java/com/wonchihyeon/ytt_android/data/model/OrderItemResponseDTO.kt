d7b5cbd94a451a5ea81555839bfd2ce08c5707e4
ylose <heungeob1003@gmail.com>
2024-12-13 19:37:24 +0900

    Feat: 주문 아이템 결과 DTO 추가 #89

---
+++ FE/app/src/main/java/com/wonchihyeon/ytt_android/data/model/OrderItemResponseDTO.kt
@@ -0,0 +1,11 @@
+package com.wonchihyeon.ytt_android.data.model
+
+data class OrderItemResponseDTO (
+    val id: String,
+    val machineId: Int,
+    val machineName: String,
+    val productCode: String,
+    val imageURL: String,
+    val quantity: Int,
+    val totalPrice: Int,
+)
